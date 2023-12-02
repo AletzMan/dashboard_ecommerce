39813ac8589cfec88796bba344e6627eea6c7512
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-12-02 17:40:55 -0600

    Add orders endpoint

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,34 +1,67 @@
-
-import { IOrder, IOrderByState } from "@/app/Types/types";
-import { generateRandomOrders, stateOrdersData } from "@/app/utils/mockdata";
-import { NextRequest, NextResponse } from "next/server";
+import { IOrder, IOrderByState, IProductData, IQuantity, ProductType } from "@/app/Types/types"
+import { pool } from "@/app/utils/database"
+import { CreateIDProduct } from "@/app/utils/functions"
+import { generateRandomOrders, productsData, stateOrdersData } from "@/app/utils/mockdata"
+import { escapeId, ResultSetHeader } from "mysql2/promise"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest, context: any) {
-    const params = request.nextUrl.searchParams
-    const paramSort = params.get("sort")
-    try {
-        /* TODO */
-        //Agregar aqui el fetch a la base de datos
-        if (paramSort === "state") {
-            const response: IOrderByState[] = stateOrdersData
-            return NextResponse.json({ response }, { status: 200 })
+  const params = request.nextUrl.searchParams
+  const paramSort = params.get("sort")
+  const paramQuantity: string = params.get("quantity") || ""
+  const search = params.get("search") || ""
+  const sort = params.get("sort") || "asc"
+  const order = params.get("order") || ""
+  const page: number = Number(params.get("page")) || 1
+  try {
+    /* TODO */
+    //Agregar aqui el fetch a la base de datos
+    if (paramSort === "state") {
+      const response: IOrderByState[] = stateOrdersData
+      return NextResponse.json({ response }, { status: 200 })
+    } else {
+      let query = "SELECT * FROM orders"
+      const pageSize = 5
+      const offset = (page - 1) * pageSize
 
-        }
-        const orders: IOrder[] = generateRandomOrders(8)
+      if (search) {
+        query += " WHERE id LIKE ? OR order_id LIKE ?"
+      }
 
-        const response = orders.sort((a, b) => {
-            if (a.orderId > b.orderId) {
-                return -1
-            }
-            if (a.orderId < b.orderId) {
-                return 1
-            }
-            return 0
-        })
+      if (order) {
+        query += ` ORDER BY ${order} ${sort}`
+      }
 
-        return NextResponse.json({ response }, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({ error }, { status: 500 })
+      query += ` LIMIT ${pageSize} OFFSET ${offset}`
+
+      // Crear un array de valores para la declaración preparada
+      const likePattern = `%${search}%`
+      const values = search ? [likePattern, likePattern] : []
+
+      const [orders] = await pool.query(query, values)
+
+      let queryCount = "SELECT COUNT (*) AS quantity FROM orders"
+      if (search) {
+        queryCount += " WHERE id LIKE ? OR order_id LIKE ?"
+      }
+      const likePatternCount = `%${search}%`
+      const valuesCount = search ? [likePatternCount, likePatternCount] : []
+
+      const response = await pool.query(queryCount, valuesCount)
+      const quantity: IQuantity[] = response[0] as IQuantity[]
+      const totalOrders = quantity[0].quantity
+      const totalPages = Math.ceil(totalOrders / pageSize)
+      const data = {
+        orders,
+        totalOrders,
+        totalPages,
+        currentPage: page,
+        pageSize,
+      }
+
+      return NextResponse.json({ data }, { status: 200 })
     }
-
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 })
+  }
 }
