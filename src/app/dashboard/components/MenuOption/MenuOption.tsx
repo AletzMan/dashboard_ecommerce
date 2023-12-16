6ca95d2f8cf0b36545d70b2bbcffde2c276758aa
Alex Garcia <alejo_2986@hotmail.com>
2023-12-15 18:06:54 -0600

    Refactor MenuOption component

--- src/app/dashboard/components/MenuOption/MenuOption.tsx
+++ src/app/dashboard/components/MenuOption/MenuOption.tsx
@@ -29,20 +29,21 @@ export function MenuOption(props: props) {
 				{options.section}
 				{options?.subSections && options?.subSections?.length > 0 && <ArrowRightIcon />}
 			</button>
-			{options.subSections && options.section === sectionName &&
+			{options.subSections && options.section === sectionName && (
 				<div className={`${styles.aside__linkOption} ${options.section === sectionName && styles.aside__linkOptionActive}`}>
 					{options.subSections?.map((subSection, index) => (
 						<Link
 							key={subSection}
 							className={`${styles.aside__sublink} ${subSectionName === subSection.replaceAll(" ", "-").toLowerCase() && styles.aside__sublinkActive}`}
-							href={`/dashboard/${options.section.toLowerCase()}/${subSection.toLowerCase()}`.replaceAll(" ", "-")}
+							href={`/dashboard/${options.section.toLowerCase()}/${subSection.toLowerCase()}`.replaceAll(" ", "-") + options.linksSubsection[index]}
 							title={`Go to section ${subSection}`}
 						>
 							<PointIcon />
 							{subSection}
 						</Link>
 					))}
-				</div>}
+				</div>
+			)}
 		</>
 	)
 }
