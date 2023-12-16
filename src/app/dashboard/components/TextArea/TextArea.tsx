6d00eca591d14c7287ff7aad63efdb5cee242f41
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-12-15 18:03:39 -0600

    Refactor TextArea component to use react-hook-form

--- src/app/dashboard/components/TextArea/TextArea.tsx
+++ src/app/dashboard/components/TextArea/TextArea.tsx
@@ -1,27 +1,36 @@
 import { ChangeEvent, ChangeEventHandler } from "react"
 import styles from "./textarea.module.scss"
+import { Control, Controller, useForm } from "react-hook-form"
 
 interface Props {
 	name: string
 	label?: string
-	value: string
+	value?: string
+	controlExt?: Control<any>
 	onChange: ChangeEventHandler<HTMLTextAreaElement>
-	error?: boolean
+	error?: string
 	isRequired?: boolean
 }
 
 export function TextArea(props: Props) {
-	const { name, label, value, onChange, error, isRequired } = props
-
+	const { name, label, value, onChange, controlExt, error, isRequired } = props
+	const { control } = useForm()
 	return (
 		<div className={styles.area}>
 			<label className={styles.area_label}>{label}</label>
-			<textarea
+			<Controller
 				name={name}
-				className={`${styles.area_text} ${isRequired && error && styles.area_textError} scrollBarStyle`}
-				value={value}
-				onChange={onChange}
-			></textarea>
+				control={controlExt || control}
+				render={({ field: { onChange, name } }) => (
+					<textarea
+						name={name}
+						className={`${styles.area_text} ${isRequired && error && styles.area_textError} scrollBarStyle`}
+						value={value}
+						onChange={onChange}
+					></textarea>
+				)}
+			/>
+			{error && <span className={styles.area_error}>{error}</span>}
 		</div>
 	)
 }
