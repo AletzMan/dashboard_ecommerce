05ead2c2a008afaaed7690a664cddb1b55001f62
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-12-15 18:03:26 -0600

    Refactor ComboBox component to use react-hook-form

--- src/app/dashboard/components/ComboBox/ComboBox.tsx
+++ src/app/dashboard/components/ComboBox/ComboBox.tsx
@@ -2,23 +2,23 @@
 import { ArrowDownLineIcon, ArrowIcon, ArrowUpIcon, CheckTwoIcon, ErrorIcon, LoadingIcon } from "@/app/SVG/componentsSVG"
 import { useState, ChangeEvent, MouseEvent, Dispatch, SetStateAction, useRef, useEffect, FC, ChangeEventHandler } from "react"
 import styles from "./formcombobox.module.scss"
-import { Control, Controller } from "react-hook-form"
+import { Control, Controller, useForm } from "react-hook-form"
 
 interface Props {
 	value: string
 	options: string[]
 	label?: string
 	name: string
-	control: Control<any>
+	controlExt?: Control<any>
 	error?: string | undefined
 	loading?: boolean
 	plaaceholder?: string
 	onValueChange: ChangeEventHandler<HTMLSelectElement>
 }
 
-export const ComboBox: FC<Props> = ({ options, name, control, error, loading, plaaceholder, label, onValueChange, value }) => {
-	const [selectOption, setSelectOption] = useState("")
-
+export const ComboBox: FC<Props> = ({ options, name, controlExt, error, loading, plaaceholder, label, onValueChange, value }) => {
+	const [selectOption, setSelectOption] = useState(value)
+	const { control } = useForm()
 	const HandleChangeValue = (e: ChangeEvent<HTMLSelectElement>, event: (...event: any[]) => void) => {
 		const newValue = e.currentTarget.value
 		event(newValue)
@@ -26,22 +26,28 @@ export const ComboBox: FC<Props> = ({ options, name, control, error, loading, pl
 		setSelectOption(newValue)
 	}
 
+	useEffect(() => {
+		setSelectOption(value)
+	}, [value])
+
+	console.log(selectOption, value)
+
 	return (
 		<div className={`${styles.combobox} ${!options && styles.comboboxDisabled}`}>
 			{label && <label className={styles.combobox_label}>{label}</label>}
 			<Controller
 				name={name}
-				control={control}
+				control={controlExt || control}
 				render={({ field: { value, onChange, name } }) => (
 					<select
 						className={`${styles.combobox__input} ${error && styles.combobox__inputError}`}
 						name={name}
 						onChange={(e) => HandleChangeValue(e, onChange)}
-						defaultValue={plaaceholder}
+						defaultValue={selectOption || plaaceholder}
 					>
 						{
-							<option className={styles.combobox_default} value={plaaceholder} disabled>
-								--{plaaceholder}--
+							<option className={styles.combobox_default} value={selectOption || plaaceholder} disabled>
+								{selectOption || plaaceholder}
 							</option>
 						}
 						{options?.map((option) => (
