da5d22a88bf2b16ae6d43af2141d95c7c58d0cda
= <=>
2024-01-06 17:27:45 -0600

    Add close menu functionality to MenuDashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,3 @@
-import { BarChart, IOptionsChart } from "./components/BarChart/BarChart"
-import { MexicoMap } from "./overview/components/MexicoMap/MexicoMap"
-import Link from "next/link"
-import styles from "./dashboard.module.scss"
-import { LatestOrders } from "./overview/components/LatestOrders/LatestOrders"
-import { ArrowRightIcon } from "../SVG/componentsSVG"
-import { TotalsView } from "./overview/components/TotalsView/TotalsView"
-import { PieChartG } from "./components/PieChart/PieChart"
-import { TopProducts } from "./overview/components/TopProducts/TopProducts"
-import axios from "axios"
-import { IOrderByState } from "../Types/types"
-import { TopSales } from "./overview/components/TopSales/TopSales"
 
 export default async function DashboardPage() {
 	return <></>
