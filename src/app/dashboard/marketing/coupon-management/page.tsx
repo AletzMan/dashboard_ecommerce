938f6f4ed1fb55977a6b093d4fb264c8536c217e
= <=>
2024-01-05 21:39:25 -0600

    Added button and coupon section

--- src/app/dashboard/marketing/coupon-management/page.tsx
+++ src/app/dashboard/marketing/coupon-management/page.tsx
@@ -1,13 +1,15 @@
 import axios from "axios"
 import styles from "./coupons.module.scss"
 import { DataGrid } from "../../components/DataGrid/DataGrid"
-import { SearchBrands } from "../../products/brands/SearchBrand"
+import { SearchSection } from "../../components/SearchSection/SearchSection"
+import { ButtonAddCoupon } from "./ButtonAddCoupon"
 
 interface ICoupon {
 	id: number
 	code: string
 	description: string
 	discount: number
+	limits: number
 	uses: number
 	start_date: string
 	end_date: string
@@ -35,25 +37,33 @@ const GetCoupons = async (params: [string, string][]) => {
 export default async function Page({ searchParams }: { searchParams: { [key: string]: string } }) {
 	const params = Object.entries(searchParams)
 	const data: IPagination = await GetCoupons(params)
-	console.log(data)
+
 	return (
-		<section className={styles.section}>
-			<SearchBrands total={data.total} placeholder="AMD, Intel, samsung, etc..." />
+		<section className={`${styles.section} scrollBarStyle`}>
+			<header className={styles.header}>
+				<SearchSection total={data.total} placeholder="Search by code or description" />
+				<ButtonAddCoupon />
+			</header>
 			<DataGrid
 				rows={data.coupons}
 				columns={[
 					{ field: "id", headerName: "ID", role: "text", width: 70 },
 					{ field: "code", headerName: "Code", role: "text", width: 130 },
 					{ field: "description", headerName: "Description", role: "text", width: "1fr" },
-					{ field: "discount", headerName: "Discount", role: "text", width: 130 },
-					{ field: "uses", headerName: "Uses", role: "text", width: 130 },
+					{ field: "discount", headerName: "Discount", role: "text", width: 100 },
+					{ field: "limits", headerName: "Limits", role: "text", width: 70 },
+					{ field: "uses", headerName: "Uses", role: "text", width: 70 },
 					{ field: "start_date", headerName: "Start Date", role: "date", width: 130 },
 					{ field: "end_date", headerName: "End Date", role: "date", width: 130 },
+					{ field: "actions", headerName: "Actions", role: "actions", width: 50 }
 				]}
 				paginacion={{
 					currentPage: data.currentPage,
 					totalPages: data.totalPages,
 				}}
+				actions={["delete"]}
+				databaseName="coupons"
+
 			/>
 		</section>
 	)
