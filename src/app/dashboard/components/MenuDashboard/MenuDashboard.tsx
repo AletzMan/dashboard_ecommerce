180850f64c1be9ae2bea53661b1b3fe0fbd07a28
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-01-06 17:27:45 -0600

    Add close menu functionality to MenuDashboard

--- src/app/dashboard/components/MenuDashboard/MenuDashboard.tsx
+++ src/app/dashboard/components/MenuDashboard/MenuDashboard.tsx
@@ -5,13 +5,15 @@ import { useRouter } from "next/navigation"
 import { MouseEvent } from "react"
 import { MenuOption } from "../MenuOption/MenuOption"
 import { MenuOptions } from "@/app/Constants/constants"
-import { Logo } from "@/app/SVG/componentsSVG"
+import { CloseIcon, Logo } from "@/app/SVG/componentsSVG"
+import { useViewMenu } from "@/app/utils/store"
 
 export function MenuDashboard() {
+	const { viewMenuDashboard, setViewMenuDashboard } = useViewMenu()
 	const router = useRouter()
 	const HandleActiveSection = (e: MouseEvent<HTMLButtonElement>) => {
 		const nameSection = e.currentTarget.name
-
+		setViewMenuDashboard(false)
 		// Establecer en true solo el nombre que coincida
 		const params = MenuOptions.filter((option) => option.section === nameSection)[0].links
 		const year = new Date().getFullYear()
@@ -19,9 +21,16 @@ export function MenuDashboard() {
 		router.push(`/dashboard/${nameSection}${params}${params && year}`)
 	}
 
+	const HandleCloseMenu = () => {
+		setViewMenuDashboard(false)
+	}
+
 	return (
-		<aside className={styles.aside}>
-			<Logo type="light" />
+		<aside className={`${styles.aside} ${viewMenuDashboard && styles.aside_view}`} >
+			<Logo className={styles.aside_logo} type="light" />
+			<button className={styles.aside_button} onClick={HandleCloseMenu} >
+				<CloseIcon />
+			</button>
 			<span className={styles.aside__title}>ADMIN PANEL</span>
 			<nav className={`${styles.aside__nav} scrollBarStyle`}>
 				{MenuOptions.map((option) => (
