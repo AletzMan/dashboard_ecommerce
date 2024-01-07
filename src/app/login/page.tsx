2ae3de9a3bb44d5c5ec22e047f8dd0723f14a90b
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-01-07 01:12:50 -0600

    Refactor login form and add form validation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,37 @@
 "use client"
 
-import Link from "next/link"
+
 import { ChangeEvent, FormEvent, useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { SnackbarProvider, enqueueSnackbar } from "notistack"
 import { signIn, useSession } from "next-auth/react"
-import axios from "axios"
 import styles from "./login.module.scss"
 import { LoadingIcon, LogInIcon } from "../SVG/componentsSVG"
 import { Button } from "../dashboard/components/Button/Button"
 import { TextFieldType } from "../Types/types"
 import HeaderHome from "../components/HeaderHome/HeaderHome"
 import { TextField } from "../dashboard/components/TextField/TextField"
+import { loginSchema } from "../validations/loginSchema"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { KeyboardEvent } from "react"
+
+interface ICredentials {
+	email: string
+	password: string
+}
 
 function FormLogin() {
 	const router = useRouter()
 	const { data: session, status } = useSession()
 	const [credentials, setCredentials] = useState({ email: "", password: "" })
-	const [error, setError] = useState({ email: true, password: true })
+	const [error, setError] = useState({ email: false, password: false })
 	const [loading, setLoading] = useState(false)
+	const { handleSubmit, formState, control } = useForm<ICredentials>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: { email: "", password: "" }
+	})
+	const { errors, isValid } = formState
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setCredentials({ ...credentials, [e.target.name]: e.target.value })
@@ -35,11 +48,11 @@ function FormLogin() {
 			email: credentials.email,
 			password: credentials.password,
 			redirect: false,
-			callbackUrl: "/dashboard",
+			callbackUrl: "/dashboard/overview",
 		})
 		console.log(response)
 		if (response?.ok) {
-			router.push("/dashboard")
+			router.push("/dashboard/overview")
 			setLoading(false)
 		}
 		if (response?.error) {
@@ -49,43 +62,53 @@ function FormLogin() {
 				password: response.error.includes("password"),
 			})
 
-			setLoading(false)
 		}
 	}
 
-	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+	const HandleLogin = () => {
 		setLoading(true)
-		e.preventDefault()
-		ValidateInfoAndSend()
+		if (isValid) {
+			ValidateInfoAndSend()
+		}
+		setLoading(false)
 	}
+
+
+	const HandleMouseEnter = () => {
+		setLoading(true)
+		if (isValid) {
+			ValidateInfoAndSend()
+		}
+		setLoading(false)
+	}
+
 	return (
 		<>
 			<SnackbarProvider autoHideDuration={4500} preventDuplicate={true} anchorOrigin={{ horizontal: "left", vertical: "top" }} />
-			<HeaderHome />
+			<HeaderHome title="e-Commerce Dashboard" />
 			<main className={styles.login}>
-				<form className={styles.login__form} onSubmit={handleSubmit}>
+				<form className={styles.login__form} onSubmit={handleSubmit(data => data)}>
 					<h2 className={styles.login__title}>Login to your account</h2>
 					<LogInIcon className={styles.login__icon} />
 					<TextField
 						textFieldProps={{
 							name: "email",
 							label: "E-mail",
-							value: credentials.email,
+							controlExt: control,
 							onChange: handleChange,
 							type: TextFieldType.Mail,
-							error: error.email,
-							isRequired: true,
+							error: errors.email?.message || error.email ? "Invalid email" : "",
 						}}
 					/>
 					<TextField
 						textFieldProps={{
 							name: "password",
 							label: "Password",
-							value: credentials.password,
+							controlExt: control,
 							onChange: handleChange,
+							onKeyDown: () => HandleMouseEnter(),
 							type: TextFieldType.Password,
-							error: error.password,
-							isRequired: true,
+							error: errors.password?.message || error.password ? "Invalid password" : "",
 						}}
 					/>
 					<Button
@@ -94,7 +117,7 @@ function FormLogin() {
 							text: !loading ? (status === "authenticated" ? "ACCESSING..." : "SIGN IN") : "",
 							iconButton: loading ? <LoadingIcon /> : undefined,
 							disabled: loading || status === "authenticated",
-							onClick() {},
+							onClick: HandleLogin,
 							type: "submit",
 						}}
 					/>
