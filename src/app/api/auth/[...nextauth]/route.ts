dea4f5b088c2da57c03ec75d4ca7380660269a0a
= <=>
2024-05-14 20:47:14 -0600

    Chore: delete unuset files END Points API

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -83,45 +83,7 @@ const handler = NextAuth({
 						}
 					}
 				}
-				console.log("LLEGA")
 				return null
-				/*
-        //
-        if (credentials?.email === "" || credentials?.password === "") {
-          throw new Error(
-            `This field ${credentials?.email === "" ? "'email'" : ""} ${credentials?.password === "" ? "'password'" : ""
-            } is required`
-          )
-        }
-        if (!IsValidMail(credentials?.email as string)) {
-          throw new Error("Please enter a valid email address")
-        }
-        //---SEARCH USER IN DATABASE---//
-        const SearchUser = async () => {
-          const result = await pool.query<IUser[]>(
-            `SELECT id, email, name, lastname, password, privileges, image FROM users WHERE email=?`,
-            [credentials?.email]
-          )
-          //console.log(result)
-          return result[0] as IUser[]
-        }
-        const userFind = await SearchUser()
-        if (userFind.length == 0) {
-          throw new Error("There is no registration for the email provided.")
-        }
-        const PASSWORD_DECRYPT = crypt.decrypt(userFind[0].password)
-        if (userFind[0].email === credentials?.email && PASSWORD_DECRYPT === credentials.password) {
-          //---WHETHER THE DATA ENTERED MATCH THOSE IN THE DATABASE---//
-          if (userFind[0].privileges === 1) {
-            return userFind[0]
-          } else {
-            throw new Error("You do not have the necessary privileges to access this page.")
-          }
-        } else {
-          //---THE PASSWORD DOES NOT MATCH THE ONE IN THE DATABASE---//
-          throw new Error("The email and password do not match")
-        }
-        */
 			},
 		}),
 	],
