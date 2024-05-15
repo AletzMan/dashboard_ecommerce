dfd04fc394ee66b269cc75e43dbf641f6437234f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-05-14 22:38:42 -0600

    Fixed: error letter dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ export default async function DashboardPage() {
 			<h2 className={styles.section_title}>Welcome</h2>
 			<Logo type="light" />
 			<p className={styles.section_p}>We are pleased to have you here. This is your command center, where you can access all the tools and resources you need to manage your projects efficiently. </p>
-			<p className={styles.section_p}> Navigate through the different sections to get detailed reports, monitor the progress of your tasks, and stay updated with all the important updates. If you have any questions or need assistance, don't hesitate to contact our support team.</p>
+			<p className={styles.section_p}> Navigate through the different sections to get detailed reports, monitor the progress of your tasks, and stay updated with all the important updates. If you have any questions or need assistance, don not hesitate to contact our support team.</p>
 		</section>
 	)
 }
